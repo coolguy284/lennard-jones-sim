0c65ce61176bfbca169adafe7337e75c191f39b5
coolguy284 <coolguy284v64@gmail.com>
2023-10-05 00:29:11 -0400

    clean up batch file
    Signed-off-by: coolguy284 <coolguy284v64@gmail.com>

--- run_rust.bat
+++ run_rust.bat
@@ -1,5 +1,5 @@
-rem sets current directory to batch file's directory
-rem https://serverfault.com/questions/95686/change-current-directory-to-the-batch-file-directory/95696#95696
-cd /D "%~dp0"
+@rem sets current directory to batch file's directory
+@rem https://serverfault.com/questions/95686/change-current-directory-to-the-batch-file-directory/95696#95696
+@cd /D "%~dp0"
 
-"generation/rust_src/target/debug/lennard_jones_rust.exe"
+@"generation/rust_src/target/debug/lennard_jones_rust.exe"
